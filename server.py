8b15a3a3223a254d8cf275770932311abb2e8792
Farouq Oyebiyi <farouqzaib@gmail.com>
2020-06-15 08:41:15 +0100

    installed package geopy and used it to calculate distance between 2 coords; london coords and query coords fetched from /users on upstream

--- server.py
+++ server.py
@@ -3,11 +3,19 @@ from flask import jsonify, make_response
 app = Flask(__name__)
 
 import requests
+import geopy.distance
+
+LONDON_COORDS = (51.509865, -0.118092)
 
 @app.route('/')
 def fetch_people():
-    r = requests.get('https://bpdts-test-app.herokuapp.com/city/London/users')
+    london = []
+    r = requests.get('https://bpdts-test-app.herokuapp.com/users')
     if r.status_code == 200:
-        return make_response(jsonify({'status': 'success', 'data': r.json()}), 200)
+        for person in r.json():
+            query_coords = (float(person['latitude']), float(person['longitude']))
+            if geopy.distance.distance(LONDON_COORDS, query_coords).miles <= 30:
+                london.append(person)
+        return make_response(jsonify({'status': 'success', 'data': london}), 200)
     else:
         return make_response(jsonify({'status': 'error', 'message': 'Upstream API did not respond with HTTP 200.'}), 502)
