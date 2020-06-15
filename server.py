ce4388a7a63f0431bb30ad1b773d6278e1376628
Farouq Oyebiyi <farouqzaib@gmail.com>
2020-06-15 08:09:06 +0100

    added code to fetch people living in london and return response as JSON if server returns HTTP 200 else return HTTP 502 and user friendly message

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import jsonify, make_response
 app = Flask(__name__)
 
 import requests
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def fetch_people():
+    r = requests.get('https://bpdts-test-app.herokuapp.com/city/London/users')
+    if r.status_code == 200:
+        return make_response(jsonify({'status': 'success', 'data': r.json()}), 200)
+    else:
+        return make_response(jsonify({'status': 'error', 'message': 'Upstream API did not respond with HTTP 200.'}), 502)
