0cf96bc37e7a7df645117aa7f91a6b9e0a059e2a
Farouq Oyebiyi <farouqzaib@gmail.com>
2020-06-16 23:21:28 +0100

    added try..catch blocks to failure points; http request for upstream api, calculation of distance between coords and parsing JSON returned by upstream API

--- server.py
+++ server.py
@@ -10,11 +10,26 @@ LONDON_COORDS = (51.509865, -0.118092)
 @app.route('/')
 def fetch_people():
     london = []
-    r = requests.get('https://bpdts-test-app.herokuapp.com/users')
+    try:
+        r = requests.get('https://bpdts-test-app.herokuapp.com/users')
+    except:
+        return make_response(jsonify({'status': 'error', 'message': 'Upstream API could not be reached.'}), 500)
+
     if r.status_code == 200:
-        for person in r.json():
+        try:
+            response = r.json()
+        except:
+            return make_response(jsonify({'status': 'error', 'message': 'Error parsing JSON object returned by upstream API.'}), 500)
+
+        for person in response:
             query_coords = (float(person['latitude']), float(person['longitude']))
-            if geopy.distance.distance(LONDON_COORDS, query_coords).miles <= 30:
+
+            try:
+                distance = geopy.distance.distance(LONDON_COORDS, query_coords).miles
+            except:
+                return make_response(jsonify({'status': 'error', 'message': 'We had a problem calculating the distance. Please try again later.'}), 500)
+                
+            if distance <= 30:
                 london.append(person)
         return make_response(jsonify({'status': 'success', 'data': london}), 200)
     else:
